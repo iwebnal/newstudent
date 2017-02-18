500e12804f80b45c923b68b3fc597e29a4af6af7
iwebnal <iwebnal@gmail.com>
2017-02-19 02:19:07 +0300

    index2

---
+++ app/includes/banerbox1.php
@@ -0,0 +1,37 @@
+<section class="ah-banerbox1">
+	<div class="ah-banerbox1-top">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-5 col-md-5 col-sm-5 col-xs-12">
+					<div class="ah-banerbox-logo">
+						<img src="img/ufa.png" class="img-responsive" alt="ufa">
+					</div>
+					<div class="ah-banerbox-city">
+						<span class="ah-banercity-span ah-bcity-span">г. Уфа</span>
+						<span class="ah-banercity-span1">ул. Ленина, д. 2, офис 89</span>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+					<span class="ah-banercity-span">График работы</span>
+					<ul class="ah-banercity-list">
+						<li><a href="#" class="ah-banercity-week1"></a></li>
+						<li><a href="#" class="ah-banercity-week2"></a></li>
+						<li><a href="#" class="ah-banercity-week3"></a></li>
+						<li><a href="#" class="ah-banercity-week4"></a></li>
+						<li><a href="#" class="ah-banercity-week5"></a></li>
+						<li><a href="#" class="ah-banercity-week6"></a></li>
+						<li><a href="#" class="ah-banercity-week7"></a></li>
+					</ul>
+					<span class="ah-banercity-span2">9:00-18:00</span>
+					<span class="ah-banercity-span2">14:00-15:00 - перерыв</span>
+				</div>
+				<div class="col-lg-3 col-md-3 col-sm-3 col-xs-12">
+					<span class="ah-banercity-span">Контакты</span>
+					<a href="#" class="ah-banercity-span3">+7 904 736-71-21</a>
+					<a href="#" class="ah-banercity-span3">info@ufadiplom.ru</a>
+					<a href="#" class="ah-banercity-span3">vk.com/diplom555rf</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
---
+++ app/includes/pageinfo.php
@@ -0,0 +1,121 @@
+<section class="ah-pageinfo">
+    <div class="container">
+    	<div class="row">
+	    	<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="ah-bredckrambs-box">
+					<ul class="ah-bredckrambs-list">
+						<li><a href="#">Главная ></a></li>
+						<li><span>Новости</span></li>
+					</ul>
+				</div>
+	    	</div>
+	    	<div class="clearfix"></div>
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<span class="ah-zakazbox-titlefirst ah-pageinfo-title">Новости</span>
+			</div>
+			<div class="clearfix"></div>
+			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+				<div class="ah-pageinfo-wrapp">
+					<div class="ah-pageinfo-imgbox">
+						<img src="img/pageinfo.jpg" class="img-responsive" alt="pageinfo">
+					</div>
+					<span class="ah-pageinfo-span">01.03.2016</span>
+					<span class="ah-pageinfo-span1">Стартует акция «Приведи друга – получи деньги на мобильный телефон»</span>
+					<p class="ah-pageinfo-text">У вас много друзей и знакомых, которые находятся в поиске помощи написания дипломных, курсовых работ? Специально для вас мы предоставляем уникальную возможность – помочь друзьям и получить за это вознаграждение!</p>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+				<div class="ah-pageinfo-wrapp">
+					<div class="ah-pageinfo-imgbox">
+						<img src="img/pageinfo1.jpg" class="img-responsive" alt="pageinfo1">
+					</div>
+					<span class="ah-pageinfo-span">01.03.2016</span>
+					<span class="ah-pageinfo-span1">Стартует акция «Приведи друга – получи деньги на мобильный телефон»</span>
+					<p class="ah-pageinfo-text">У вас много друзей и знакомых, которые находятся в поиске помощи написания дипломных, курсовых работ? Специально для вас мы предоставляем уникальную возможность – помочь друзьям и получить за это вознаграждение!</p>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+				<div class="ah-pageinfo-wrapp">
+					<div class="ah-pageinfo-imgbox">
+						<img src="img/pageinfo.jpg" class="img-responsive" alt="pageinfo">
+					</div>
+					<span class="ah-pageinfo-span">01.03.2016</span>
+					<span class="ah-pageinfo-span1">Стартует акция «Приведи друга – получи деньги на мобильный телефон»</span>
+					<p class="ah-pageinfo-text">У вас много друзей и знакомых, которые находятся в поиске помощи написания дипломных, курсовых работ? Специально для вас мы предоставляем уникальную возможность – помочь друзьям и получить за это вознаграждение!</p>
+				</div>
+			</div>
+			<div class="clearfix"></div>
+			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+				<div class="ah-pageinfo-wrapp">
+					<div class="ah-pageinfo-imgbox">
+						<img src="img/pageinfo.jpg" class="img-responsive" alt="pageinfo">
+					</div>
+					<span class="ah-pageinfo-span">01.03.2016</span>
+					<span class="ah-pageinfo-span1">Стартует акция «Приведи друга – получи деньги на мобильный телефон»</span>
+					<p class="ah-pageinfo-text">У вас много друзей и знакомых, которые находятся в поиске помощи написания дипломных, курсовых работ? Специально для вас мы предоставляем уникальную возможность – помочь друзьям и получить за это вознаграждение!</p>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+				<div class="ah-pageinfo-wrapp">
+					<div class="ah-pageinfo-imgbox">
+						<img src="img/pageinfo1.jpg" class="img-responsive" alt="pageinfo1">
+					</div>
+					<span class="ah-pageinfo-span">01.03.2016</span>
+					<span class="ah-pageinfo-span1">Стартует акция «Приведи друга – получи деньги на мобильный телефон»</span>
+					<p class="ah-pageinfo-text">У вас много друзей и знакомых, которые находятся в поиске помощи написания дипломных, курсовых работ? Специально для вас мы предоставляем уникальную возможность – помочь друзьям и получить за это вознаграждение!</p>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+				<div class="ah-pageinfo-wrapp">
+					<div class="ah-pageinfo-imgbox">
+						<img src="img/pageinfo.jpg" class="img-responsive" alt="pageinfo">
+					</div>
+					<span class="ah-pageinfo-span">01.03.2016</span>
+					<span class="ah-pageinfo-span1">Стартует акция «Приведи друга – получи деньги на мобильный телефон»</span>
+					<p class="ah-pageinfo-text">У вас много друзей и знакомых, которые находятся в поиске помощи написания дипломных, курсовых работ? Специально для вас мы предоставляем уникальную возможность – помочь друзьям и получить за это вознаграждение!</p>
+				</div>
+			</div>
+			<div class="clearfix"></div>
+			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+				<div class="ah-pageinfo-wrapp">
+					<div class="ah-pageinfo-imgbox">
+						<img src="img/pageinfo.jpg" class="img-responsive" alt="pageinfo">
+					</div>
+					<span class="ah-pageinfo-span">01.03.2016</span>
+					<span class="ah-pageinfo-span1">Стартует акция «Приведи друга – получи деньги на мобильный телефон»</span>
+					<p class="ah-pageinfo-text">У вас много друзей и знакомых, которые находятся в поиске помощи написания дипломных, курсовых работ? Специально для вас мы предоставляем уникальную возможность – помочь друзьям и получить за это вознаграждение!</p>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+				<div class="ah-pageinfo-wrapp">
+					<div class="ah-pageinfo-imgbox">
+						<img src="img/pageinfo1.jpg" class="img-responsive" alt="pageinfo1">
+					</div>
+					<span class="ah-pageinfo-span">01.03.2016</span>
+					<span class="ah-pageinfo-span1">Стартует акция «Приведи друга – получи деньги на мобильный телефон»</span>
+					<p class="ah-pageinfo-text">У вас много друзей и знакомых, которые находятся в поиске помощи написания дипломных, курсовых работ? Специально для вас мы предоставляем уникальную возможность – помочь друзьям и получить за это вознаграждение!</p>
+				</div>
+			</div>
+			<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+				<div class="ah-pageinfo-wrapp">
+					<div class="ah-pageinfo-imgbox">
+						<img src="img/pageinfo.jpg" class="img-responsive" alt="pageinfo">
+					</div>
+					<span class="ah-pageinfo-span">01.03.2016</span>
+					<span class="ah-pageinfo-span1">Стартует акция «Приведи друга – получи деньги на мобильный телефон»</span>
+					<p class="ah-pageinfo-text">У вас много друзей и знакомых, которые находятся в поиске помощи написания дипломных, курсовых работ? Специально для вас мы предоставляем уникальную возможность – помочь друзьям и получить за это вознаграждение!</p>
+				</div>
+			</div>
+			<div class="clearfix"></div>
+			<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+				<div class="ah-pageinfo-pagination">
+					<ul class="ah-pageinfo-paglist">
+						<li><a href="#">1</a></li>
+						<li><a href="#">2</a></li>
+						<li><a href="#">>></a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
